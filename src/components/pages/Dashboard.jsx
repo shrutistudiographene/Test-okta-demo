b6bbd0d7d7f305f7ddde180c7ae81bd236ac0c93
Shruti Mishra <shruti@spokemarketplace.com>
2022-09-19 11:31:08 +0530

    reset password

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -6,11 +6,19 @@ const Dashboard = () => {
     const { authState } = useOktaAuth();
     const [currentUserName] = useState(authState?.idToken?.claims?.name);
     const [currentUserEmail] = useState(authState?.idToken?.claims?.email);
+
     return <div>
         <h1>Dashboard</h1>
         <p>Name :{currentUserName}</p>
         <p>Email : {currentUserEmail}</p>
+
+        <button className='btn btn-light btn-lg'>
+            <a href="https://trial-2354760.okta.com/signin/forgot-password">forgot password</a> 
+        </button>
     </div>;
 }
 
 export default Dashboard;
+// https://abc.xyz.com/forgetpwd 
+// example.app.com/signin/password-reset
+// https://trial-2354760.okta.com/signin/forgot-password
